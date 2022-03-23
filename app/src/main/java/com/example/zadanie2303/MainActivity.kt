af464ecf4237468728e666db33041da578571823
dawidrzuk <dawidrzukk@gmail.com>
2022-03-23 07:51:55 +0100

    wybor plci + wiek + pola wyboru + przyciski + wyswietlanie tekstu deklaracja zmiennych wyboru

--- app/src/main/java/com/example/zadanie2303/MainActivity.kt
+++ app/src/main/java/com/example/zadanie2303/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.zadanie2303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mezczyzna = findViewById<RadioButton>(R.id.mezczyzna)
+        val kobieta = findViewById<RadioButton>(R.id.kobieta)
+        val wiek = findViewById<EditText>(R.id.wiek)
+        val sport = findViewById<CheckBox>(R.id.checkBox)
+        val muzyka = findViewById<CheckBox>(R.id.checkBox2)
+        val turystyka = findViewById<CheckBox>(R.id.checkBox3)
+        val gry = findViewById<CheckBox>(R.id.checkBox4)
+
+
+
     }
+
 }
